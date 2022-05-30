a4638f165b611758f1cc6a26d24086a62935cb97
star14ms <danalstj@naver.com>
2022-05-30 14:33:03 +0900

    Add argparse, Update Code Bugs

--- test.py
+++ test.py
@@ -2,15 +2,62 @@ from data import KoSyllableDataset
 from model import KoCtoP
 
 import torch
+from torch import nn
 from torch.utils.data import DataLoader
-import random
 
 from utils.unicode import join_jamos
-from utils.rich import console
+from utils.rich import new_progress, console
 from tools import to_chr
 
 
-# file_name = ''
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--load-model', type=str, dest='load_model',
+                        default='save/KoCtoP-acc_0.1159-loss_0.104213-380000.pth',
+                        help='model weight path to load')
+parser.add_argument('--batch-size', type=int, dest='batch_size',
+                        default=50,
+                        help='batch size')
+args = parser.parse_args()
+
+
+
+def test(model, test_loader, loss_fn, progress):
+    model.eval()
+    batch_size = test_loader.batch_size
+    size = len(test_loader.dataset)
+    task_id = progress.add_task(f'iter {batch_size}/{size}', total=size)
+
+    test_loss, correct, current = 0, 0, 0
+    
+    with torch.no_grad():
+        for iter, (x, t) in enumerate(test_loader):
+            x = x.to(device)
+            yi, ym, yf = model(x)
+            yi, ym, yf = yi.cpu(), ym.cpu(), yf.cpu()
+            
+            ti, tm, tf = t.values()
+            loss = loss_fn(yi, ti) + loss_fn(ym, tm) + loss_fn(yf, tf)
+            test_loss += loss.item()
+            
+            ones = torch.ones([len(x)])
+            mask_i = (yi.argmax(1) == ti)
+            mask_m = (ym.argmax(1) == tm)
+            mask_f = (yf.argmax(1) == tf)
+            correct += (ones * mask_i * mask_m * mask_f).sum().item()
+    
+            current += len(x)
+            progress.update(task_id, description=f'iter {current}/{size}', advance=len(x))
+
+            if iter % 10 == 0:
+                progress.log(f"loss: {loss.item():>6f} acc: {correct/current*100:>0.3f}%")
+
+    test_loss /= current
+    correct /= current * 100
+    progress.log(f"Test Error: \n Accuracy: {(correct):>0.3f}%, Avg loss: {test_loss:>6f} \n")
+    progress.remove_task(task_id)
+
+    return test_loss, correct
 
 
 def predict(dataset, idx, model, device, verbose=True):
@@ -34,30 +81,31 @@ def predict(dataset, idx, model, device, verbose=True):
         label_yf = to_chr['f'][t['final']]
     
         if verbose:
-            console.print('예측: {} 정답: {}'.format(char, join_jamos(label_yi + label_ym + label_yf)))
+            console.log('예측: {} 정답: {}'.format(char, join_jamos(label_yi + label_ym + label_yf)))
     
     elif verbose:
-        console.print('예측: {}'.format(char))
+        console.log('예측: {}'.format(char))
 
     return char
 
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print("Using {} device\n".format(device))
-
-
-batch_size = 10
-test_set = KoSyllableDataset(train=False)
-test_loader = DataLoader(test_set, batch_size, shuffle=True)
-
-
-model = KoCtoP().to(device)
-# model.load_state_dict(torch.load(file_name))
-
-
-model.eval()
-
-with torch.no_grad():
-    while True:
-        idx = random.randint(0, len(test_set)-1)
-        predict(test_set, 0, model, device, verbose=True)
+if __name__ == '__main__':
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    console.log("Using [green]{}[/green] device\n".format(device))
+    
+    
+    batch_size = args.batch_size
+    test_set = KoSyllableDataset(train=False)
+    test_loader = DataLoader(test_set, batch_size, shuffle=True)
+    loss_fn = nn.CrossEntropyLoss()
+    
+    
+    model = KoCtoP().to(device)
+    model.load_state_dict(torch.load(args.load_model))
+    console.log('모델 로드 완료!')
+    
+    
+    model.eval()
+    
+    with new_progress() as progress:
+        test(model, test_loader, loss_fn, progress)
--- train.py
+++ train.py
@@ -2,15 +2,27 @@ from data import KoSyllableDataset
 from model import KoCtoP
 from utils.plot import set_font
 from utils.rich import new_progress, console
+from utils.utils import makedirs
+from test import test
 
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
 import os
-import math
+import argparse
 
 from rich.traceback import install
 install()
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--load-model', type=str, dest='load_model',
+                        default=None,
+                        help='model weight path to load')
+parser.add_argument('--batch-size', type=int, dest='batch_size',
+                        default=50,
+                        help='batch size')
+args = parser.parse_args()
 
 
 set_font(family='BM JUA_TTF')
@@ -21,7 +33,7 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 console.log("Using [green]{}[/green] device\n".format(device))
 
 
-batch_size = 50
+batch_size = args.batch_size
 train_set = KoSyllableDataset() # syllable: 음절
 test_set = KoSyllableDataset(train=False)
 train_loader = DataLoader(train_set, batch_size, shuffle=True)
@@ -29,10 +41,16 @@ test_loader = DataLoader(test_set, batch_size, shuffle=True)
 console.log(f'데이터 로드 완료! (train set: {len(train_set)} / test set: {len(test_set)})')
 
 
+save_dir = 'save/'
+makedirs(save_dir)
+
+file_name = args.load_model
 model = KoCtoP().to(device)
 loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
-console.log(f'모델 준비 완료!')
+if file_name:
+    model.load_state_dict(torch.load(save_dir+file_name))
+console.log('모델 {} 완료!'.format('로드' if file_name else '준비'))
 
 
 def train(model, train_loader, loss_fn, optimizer, progress):
@@ -42,7 +60,7 @@ def train(model, train_loader, loss_fn, optimizer, progress):
 
     task_id = progress.add_task(f'iter {batch_size}/{size}', total=size)
 
-    train_loss, correct = 0, 0
+    train_loss, correct, current = 0, 0, 0
 
     for iter, (x, t) in enumerate(train_loader):
         x = x.to(device)
@@ -57,61 +75,30 @@ def train(model, train_loader, loss_fn, optimizer, progress):
 
         train_loss += loss.item()
         
-        ones = torch.ones([batch_size])
+        ones = torch.ones([len(x)])
         mask_i = (yi.argmax(1) == ti)
         mask_m = (ym.argmax(1) == tm)
         mask_f = (yf.argmax(1) == tf)
         correct += (ones * mask_i * mask_m * mask_f).sum().item()
 
-        current = (iter+1) * len(x)
-        progress.update(task_id, description=f'iter {current}/{size}', advance=1)
+        current += len(x)
+        progress.update(task_id, description=f'iter {current}/{size}', advance=len(x))
         
         if iter % 10 == 0:
             progress.log(f"loss: {loss.item():>6f}")
 
+        if current % 10000 == 0:
+            avg_loss = train_loss / current
+            avg_acc = correct / current * 100
+            file_name = model.__class__.__name__ + f'-acc_{(avg_acc):>0.3f}%-loss_{avg_loss:>6f}-{current}.pth'
+            torch.save(model.state_dict(), save_dir+file_name)
+        
+            progress.log(f'Saved PyTorch Model State to {save_dir+file_name}')
     
     progress.remove_task(task_id)
-    train_loss /= size
-    correct /= size * 100
+    train_loss /= current
+    correct /= current * 100
     return train_loss, correct
-
-
-def test(model, test_loader, loss_fn, progress):
-    model.eval()
-    batch_size = test_loader.batch_size
-    size = len(test_loader.dataset)
-    task_id = progress.add_task(f'iter {batch_size}/{size}', total=size)
-
-    test_loss, correct = 0, 0
-    
-    with torch.no_grad():
-        for iter, (x, t) in enumerate(test_loader):
-            x = x.to(device)
-            yi, ym, yf = model(x)
-            yi, ym, yf = yi.cpu(), ym.cpu(), yf.cpu()
-            
-            ti, tm, tf = t.values()
-            loss = loss_fn(yi, ti) + loss_fn(ym, tm) + loss_fn(yf, tf)
-            test_loss += loss.item()
-            
-            ones = torch.ones([batch_size])
-            mask_i = (yi.argmax(1) == ti)
-            mask_m = (ym.argmax(1) == tm)
-            mask_f = (yf.argmax(1) == tf)
-            correct += (ones * mask_i * mask_m * mask_f).sum().item()
-    
-            current = (iter+1) * len(x)
-            progress.update(task_id, description=f'iter {current}/{size}', advance=1)
-
-            if iter % 10 == 0:
-                progress.log(f"loss: {loss.item():>6f}")
-
-    test_loss /= size
-    correct /= size * 100
-    progress.log(f"Test Error: \n Accuracy: {(correct):>0.2f}%, Avg loss: {test_loss:>8f} \n")
-    progress.remove_task(task_id)
-
-    return test_loss, correct
 
 
 epochs = 1
@@ -125,7 +112,7 @@ with new_progress() as progress:
         
         progress.update(task_id, description=f'epoch {epoch}/{epochs}', advance=1)
 
-        file_name = model.__class__.__name__ + f' Acc_{(test_acc):>0.2f}-Avg loss_{test_loss:>8f}.pth'
-        torch.save(model.state_dict(), file_name)
+        file_name = model.__class__.__name__ + f'-acc_{(test_acc):>0.3f}%-loss_{test_loss:>6f}.pth'
+        torch.save(model.state_dict(), save_dir+file_name)
         
-        progress.log(f'Saved PyTorch Model State to {file_name}.pth')
+        progress.log(f'Saved PyTorch Model State to {save_dir+file_name}')

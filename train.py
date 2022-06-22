eb9b40fe8de6a910d5f7971c6cdc5d992fe26917
star14ms <danalstj@naver.com>
2022-06-23 02:46:39 +0900

    Add Trainer Class
    - Clean Train Code
    - Save Training Info

--- train.py
+++ train.py
@@ -1,18 +1,18 @@
-from data import KoSyllableDataset
-from model import KoCtoP
-from utils.plot import set_font
-from utils.rich import new_progress, console
-from utils.utils import makedirs
-from test import test
-
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
 import os
 import argparse
-
+import pandas as pd
 from rich.traceback import install
 install()
+
+from data import KoSyllableDataset
+from model import KoCtoP, KoCtoPLarge
+from utils.plot import set_font
+from utils.rich import new_progress, console
+from utils.utils import makedirs
+from test import test
 
 
 parser = argparse.ArgumentParser()
@@ -39,11 +39,10 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 console.log("Using [green]{}[/green] device\n".format(device))
 
 
-batch_size = args.batch_size
 train_set = KoSyllableDataset()
 test_set = KoSyllableDataset(train=False)
-train_loader = DataLoader(train_set, batch_size, shuffle=True)
-test_loader = DataLoader(test_set, batch_size, shuffle=True)
+train_loader = DataLoader(train_set, args.batch_size, shuffle=True)
+test_loader = DataLoader(test_set, args.batch_size, shuffle=True)
 console.log(f'데이터 로드 완료! (train set: {len(train_set)} / test set: {len(test_set)})')
 
 
@@ -52,7 +51,7 @@ makedirs(save_dir)
 
 file_name = args.load_model
 start = int(file_name.split('-')[-1].replace('.pth','')) if file_name else 0
-model = KoCtoP().to(device)
+model = KoCtoPLarge().to(device)
 loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 if file_name:
@@ -60,75 +59,105 @@ if file_name:
 console.log('모델 {} 완료!'.format('로드' if file_name else '준비'))
 
 
-def train(model, train_loader, loss_fn, optimizer, print_every, progress):
-    model.train()
-    device = 'cuda' if next(model.parameters()).is_cuda else 'cpu'
-    size = len(train_loader.dataset)
-    batch_size = train_loader.batch_size
+class Trainer:
+    TRAIN_STEP_RESULT_PATH = "train_step_result.csv"
+    train_step_result = {'n_learn':[], 'loss': [], 'acc': []}
 
-    task_id = progress.add_task(f'iter {batch_size}/{size}', total=size)
+    def __init__(self, train_loader, loss_fn, optimizer, device, print_every, save_dir):
+        self.train_loader = train_loader
+        self.loss_fn = loss_fn
+        self.optimizer = optimizer
+        self.device = device
+        self.print_every = print_every
+        self.save_dir = save_dir
+        self.progress = new_progress()
+  
 
-    train_loss, correct, current = 0, 0, 0
-    correct_per_print, current_per_print = 0, 0
-
-    for iter, (x, t) in enumerate(train_loader):
-        x = x.to(device)
-        yi, ym, yf = model(x)
-        yi, ym, yf = yi.cpu(), ym.cpu(), yf.cpu()
-        
-        ti, tm, tf = t.values()
-        loss = loss_fn(yi, ti) + loss_fn(ym, tm) + loss_fn(yf, tf)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        train_loss += loss.item()
-        
-        ones = torch.ones([len(x)])
-        mask_i = (yi.argmax(1) == ti)
-        mask_m = (ym.argmax(1) == tm)
-        mask_f = (yf.argmax(1) == tf)
-        correct_batch = (ones * mask_i * mask_m * mask_f).sum().item()
-        
-        correct += correct_batch
-        correct_per_print += correct_batch
-        current += len(x)
-        current_per_print += len(x)
-
-        progress.update(task_id, description=f'iter {current}/{size}', advance=len(x))
-        
-        if iter % print_every == 0:
-            avg_acc = correct_per_print / current_per_print * 100
-            progress.log(f"loss: {loss.item()/len(x):>6f} | acc: {avg_acc:>0.1f}%")
-            correct_per_print = 0
-            current_per_print = 0
-
-        if current % 10000 == 0:
-            avg_loss = train_loss / current
-            avg_acc = correct / current * 100
-            file_name = model.__class__.__name__ + f'-acc_{avg_acc:>0.2f}%-loss_{avg_loss:>6f}-{start+current}.pth'
-            torch.save(model.state_dict(), save_dir+file_name)
-        
-            progress.log(f'Saved PyTorch Model State to {save_dir+file_name}')
+    def train(self, model, batch_size, epochs):
+        self.progress.start()
+        task_id = self.progress.add_task(f'epoch 1/{epochs}', total=epochs)
     
-    progress.remove_task(task_id)
-    train_loss /= current
-    correct /= current
-    return train_loss, correct * 100
-
-
-epochs = args.epochs
-
-with new_progress() as progress:
-    task_id = progress.add_task(f'epoch 1/{epochs}', total=epochs)
-
-    for epoch in range(1, epochs+1):
-        train_loss, train_acc = train(model, train_loader, loss_fn, optimizer, args.print_every, progress)
-        test_loss, test_acc = test(model, train_loader, loss_fn, progress)
+        for epoch in range(1, epochs+1):
+            train_loss, train_acc = self.train_epoch(model, batch_size)
+            test_loss, test_acc = test(model, test_loader, self.loss_fn, self.progress)
+            
+            self.progress.update(task_id, description=f'epoch {epoch}/{epochs}', advance=1)
+    
+            file_name = model.__class__.__name__ + f'-acc_{(test_acc):>0.3f}%-loss_{test_loss:>6f}.pth'
+            torch.save(model.state_dict(), self.save_dir+file_name)
+            
+            self.progress.log(f'Saved PyTorch Model State to {self.save_dir+file_name}')
         
-        progress.update(task_id, description=f'epoch {epoch}/{epochs}', advance=1)
+        self.progress.stop()
 
-        file_name = model.__class__.__name__ + f'-acc_{(test_acc):>0.3f}%-loss_{test_loss:>6f}.pth'
-        torch.save(model.state_dict(), save_dir+file_name)
+
+    def train_epoch(self, model, batch_size):
+        model.train()
+        size = len(self.train_loader.dataset)
+    
+        task_id = self.progress.add_task(f'iter {batch_size}/{size}', total=size)
+    
+        train_loss, correct, current = 0, 0, 0
+        train_loss_per_print, correct_per_print, current_per_print = 0, 0, 0
+    
+        for iter, (x, t) in enumerate(self.train_loader):
+            x = x.to(self.device)
+            yi, ym, yf = model(x)
+            yi, ym, yf = yi.cpu(), ym.cpu(), yf.cpu()
+            
+            ti, tm, tf = t.values()
+            loss = self.loss_fn(yi, ti) + self.loss_fn(ym, tm) + self.loss_fn(yf, tf)
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+    
+            train_loss += loss.item()
+            train_loss_per_print += loss.item()
+            
+            ones = torch.ones([len(x)])
+            mask_i = (yi.argmax(1) == ti)
+            mask_m = (ym.argmax(1) == tm)
+            mask_f = (yf.argmax(1) == tf)
+            correct_batch = (ones * mask_i * mask_m * mask_f).sum().item()
+            
+            correct += correct_batch
+            correct_per_print += correct_batch
+            current += len(x)
+            current_per_print += len(x)
+    
+            self.progress.update(task_id, description=f'iter {current}/{size}', advance=len(x))
+            
+            if (iter+1) % self.print_every == 0:
+                avg_loss = train_loss_per_print / current_per_print
+                avg_acc = correct_per_print / current_per_print * 100
+                self.progress.log(f"loss: {avg_loss:>6f} | acc: {avg_acc:>0.1f}%")
+                self.save_step_result(self.train_step_result, current, avg_loss, avg_acc)
+                train_loss_per_print = 0
+                correct_per_print = 0
+                current_per_print = 0
+                
+            if current % 10000 == 0:
+                avg_loss = train_loss / current
+                avg_acc = correct / current * 100
+                file_name = model.__class__.__name__ + f'-acc_{avg_acc:>0.2f}%-loss_{avg_loss:>6f}-{start+current}.pth'
+                torch.save(model.state_dict(), self.save_dir+file_name)
+            
+                self.progress.log(f'Saved PyTorch Model State to {self.save_dir+file_name}')
         
-        progress.log(f'Saved PyTorch Model State to {save_dir+file_name}')
+        self.progress.remove_task(task_id)
+        train_loss /= current
+        correct /= current
+        return train_loss, correct * 100
+    
+
+    def save_step_result(self, train_step_result: dict, current: int, loss: float, acc: float) -> None:
+        train_step_result["n_learn"].append(current)
+        train_step_result["loss"].append(f'{loss:>6f}')
+        train_step_result["acc"].append(f'{acc:>0.1f}')
+        
+        train_step_df = pd.DataFrame(train_step_result)
+        train_step_df.to_csv(self.save_dir+Trainer.TRAIN_STEP_RESULT_PATH, encoding="UTF-8", index=False)
+
+
+trainer = Trainer(train_loader, loss_fn, optimizer, device, args.print_every, save_dir)
+trainer.train(model, args.batch_size, args.epochs)

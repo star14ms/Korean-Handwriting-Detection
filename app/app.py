794d6d036e13963dd93c57275625340854e13fb3
star14ms <danalstj@naver.com>
2022-06-30 21:08:19 +0900

    Add API

---
+++ app/app.py
@@ -0,0 +1,95 @@
+import os
+import json
+import re
+import time
+import socket
+from flask import Flask, request, jsonify, abort
+from flask_cors import CORS
+from werkzeug.utils import secure_filename
+import urllib.request
+from cairosvg import svg2png
+
+
+UPLOAD_FOLDER = 'app/image/'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'svg'}
+
+# 서버 접속 정보
+host = "127.0.0.1"  # 서버 IP 주소
+port = 5050  # 서버 통신 포트
+
+# Flask 어플리케이션
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+CORS(app, resources={'/detect': {'origins': 'http://localhost:19006' }}, )
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def get_file_path(filename=None):
+    date = time.strftime('%y%m%d', time.localtime())
+    save_dir = app.config['UPLOAD_FOLDER']+date
+    os.makedirs(save_dir, exist_ok=True)
+
+    if filename is None: 
+        filename = str(int(time.time())) + '.png'
+    filename = secure_filename(filename)
+    assert allowed_file(filename)
+    filepath = os.path.join(save_dir, filename)
+    
+    return filepath
+
+
+# 서버와 통신
+def _detect(img_path):
+    # 서버 연결
+    mySocket = socket.socket()
+    mySocket.connect((host, port))
+
+    json_data = {
+        'img_path': img_path,
+    }
+    message = json.dumps(json_data)
+    mySocket.send(message.encode())
+
+    data = mySocket.recv(2048).decode()
+    ret_data = json.loads(data)
+
+    mySocket.close()
+
+    return ret_data
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'hello', 200
+
+
+@app.route('/detect', methods=['POST'])
+def detect():
+    try:
+        if 'file' not in request.files:
+            data = request.data.decode('utf-8')
+            response = urllib.request.urlopen(data)
+            svg_code = response.file.read()
+            filepath = get_file_path()
+            svg2png(bytestring=svg_code, write_to=filepath)
+        else:
+            file = request.files['file']
+            filepath = get_file_path(file.filename)
+            file.save(filepath)
+            
+        ret = _detect(filepath)
+        return jsonify(ret)
+    
+    except Exception as e:
+        # 오류 발생시 500 오류
+        print(repr(e))
+        abort(500)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ app/bot.py
@@ -0,0 +1,101 @@
+import threading
+import json
+import socket
+import os
+import PIL.Image as Image
+
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from torch import tensor, load, float32
+from kohwctop.model import KoCtoP
+from kohwctop.transform import Resize
+from kohwctop.test import predict
+from utils.rich import console
+
+
+def to_client(conn, addr):
+    try:
+        # 데이터 수신
+        read = conn.recv(2048)  # 수신 데이터가 있을 때 까지 블로킹
+        console.log('===========================')
+        console.log('Connection from: %s' % str(addr))
+
+        if read is None or not read:
+            # 클라이언트 연결이 끊어지거나, 오류가 있는 경우
+            console.log('클라이언트 연결 끊어짐')
+            exit(0)
+
+        recv_json_data = json.loads(read.decode())
+        img_path = recv_json_data['img_path']
+
+        img = Image.open(img_path).convert('L')
+        answer = detect(img)
+
+        response = {
+            "answer": answer,
+        }
+        
+        console.log('예측:', answer)
+        message = json.dumps(response)
+        conn.send(message.encode())
+
+    except Exception as ex:
+        console.log(ex)
+
+    finally:
+        conn.close()
+
+
+def detect(img):
+    model = KoCtoP()
+    model.load_state_dict(load('model.pt'))
+
+    resize = Resize()
+    img = resize(img)
+    answer = predict(img, None, model)
+
+    return answer
+
+
+class BotServer:
+    def __init__(self, srv_port, listen_num):
+        self.port = srv_port
+        self.listen = listen_num
+        self.mySock = None
+
+    # sock 생성
+    def create_sock(self):
+        self.mySock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.mySock.bind(("0.0.0.0", int(self.port)))
+        self.mySock.listen(int(self.listen))
+        return self.mySock
+
+    # client 대기
+    def ready_for_client(self):
+        return self.mySock.accept()
+
+    # sock 반환
+    def get_sock(self):
+        return self.mySock
+
+
+if __name__ == '__main__':
+
+    port = 5050
+    listen = 32
+
+    # 봇 서버 동작
+    bot = BotServer(port, listen)
+    bot.create_sock()
+    console.log("bot start")
+
+    while True:
+        conn, addr = bot.ready_for_client()
+
+        client = threading.Thread(target=to_client, args=(
+            conn,
+            addr,
+        ))
+        client.start()

fcd26d75cb63c5d470b5b7891f0223302b37cf58
star14ms <danalstj@naver.com>
2022-07-03 21:45:33 +0900

    Update Transform Class - Resize
    - min crop size
    - Crop to Square

--- kohwctop/transform.py
+++ kohwctop/transform.py
@@ -9,9 +9,10 @@ to_tensor = ToTensor()
 
 
 class Resize():
-    def __init__(self, size=(64,64), resample=Image.LANCZOS):
+    def __init__(self, size=(64,64), min_crop_size=32, resample=Image.LANCZOS):
         self.size = size
         self.resample = resample
+        self.min_crop_size = min_crop_size
 
     def __call__(self, img):
         if isinstance(img, (np.ndarray, torch.Tensor)):
@@ -19,7 +20,7 @@ class Resize():
 
         arr = to_tensor(img)
         # img.show()
-        crop_idxs = get_idxs_to_crop(arr)
+        crop_idxs = get_idxs_to_crop(arr, self.min_crop_size)
         img = img.crop(crop_idxs)
         # img.show()
         img = img.resize(self.size, self.resample)
@@ -29,7 +30,7 @@ class Resize():
         return arr
 
 
-def get_idxs_to_crop(x): 
+def get_idxs_to_crop(x, min_crop_size=32): 
     row_len = x.shape[2] # (C, H, (W))
     col_len = x.shape[1] # (C, (H), W)
     
@@ -48,5 +49,17 @@ def get_idxs_to_crop(x):
     xl = 0
     while torch.sum(x[:, :, xl:xl+1]) == 0 and xl+1 < xr:
         xl += 1
+
+    while xr-xl < min_crop_size or (yb-yt) - (xr-xl) > 2:
+        if xl-1 >= 0:
+            xl -= 1
+        if xr+1 <= row_len:
+            xr += 1
+
+    while yb-yt < min_crop_size or (xr-xl) - (yb-yt) > 2:
+        if yt-1 >= 0:
+            yt -= 1
+        if yb+1 <= col_len:
+            yb += 1
 
     return xl, yt, xr, yb
